15a4dab0bfa7df6edb2992a0c7dd928736056948
Victoria <vikshab2010@hotmail.com>
2015-10-08 14:00:46 -0700

    Added model validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :user_name, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: /@/
 end
