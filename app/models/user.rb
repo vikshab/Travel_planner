989ccd42f6f789e6216bab99cd6d2e0b98fa2f46
Victoria <vikshab2010@hotmail.com>
2015-10-08 14:13:33 -0700

    Added has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase}
   validates :user_name, presence: true, uniqueness: { case_sensitive: false }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: /@/
+  has_secure_password
 end
