012012a8e4a594e412dc5d26d9116fdfa52dbb92
Victoria <vikshab2010@hotmail.com>
2015-10-09 09:52:28 -0700

    Generated controller users, seeded table users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,14 @@
 class SessionsController < ApplicationController
   def destroy
-    session[:user_id] = nil
-    flash[:success] = "Signed out!"
-    redirect_to root_path
+    # session[:user_id] = nil
+    # flash[:success] = "Signed out!"
+    # redirect_to root_path
   end
 
   def create
-    @user = User.find(id)
-    session[:user_id] = @user.id
-    flash[:success] = "You've been signed in, #{ @user.name }!"
+    # @user = User.find(id)
+    # session[:user_id] = @user.id
+    # flash[:success] = "You've been signed in, #{ @user.name }!"
     # redirect_to root_url
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -16,4 +16,14 @@ ActiveRecord::Schema.define(version: 20151007172447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string   "user_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "phone"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "uid"
+  end
+
 end
