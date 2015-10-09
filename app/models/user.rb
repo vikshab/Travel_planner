ceaf22cb9786a6c4ca0d4ddf046eb3a3bef34f47
Victoria <vikshab2010@hotmail.com>
2015-10-08 17:33:15 -0700

    Seeding data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase}
+  # before_save { self.email = email.downcase}
   validates :user_name, presence: true, uniqueness: { case_sensitive: false }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: /@/
   has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+CSV.foreach("db/users.csv", headers: true) do |row|
+
+   User.create(
+     user_name: row[0],
+     email: row[1],
+     password: row[2],
+     password_confirmation: row[3],
+     phone: row[4]
+   )
+  end
