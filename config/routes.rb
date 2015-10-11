4912b23948bd7fad996af34aa381b036f4def9cd
Victoria <vikshab2010@hotmail.com>
2015-10-11 14:55:56 -0700

    Added style for homepage Added current_user and require_login methods in application_controller Added create, create_facebook and destroy methods in sessions_controller Added new, create, show and private method user_params in users_controller Added about method in welcome controller Added validation and self.create_with_omniauth method in user model Moved code for sidebar to layouts/application Added 'new' view page for log in (sessions/new.html.erb) Created partial for title - shared/_title.html.erb Created 'new' form for new users - users/new.html.erb Created 'show' page for logged in users - users/show.html.erb Created 'about' page - welcome/about.html.erb Added a new file - omniauth.rb for facebook_omniauth Added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root "welcome#index"
-
+  get "/about", to: "welcome#about", as: "about"
   # Authorization route
   get "auth/facebook/callback" => "sessions#create_facebook"
+  get    "/login", to: "sessions#new"
+  post   "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy", as: "logout"
+
+  resource :users, only: [:new, :create]
+
+  get '/:user/dashboard', to: "users#show", as: "dashboard"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
